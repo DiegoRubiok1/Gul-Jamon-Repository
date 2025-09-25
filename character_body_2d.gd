227654ff69e2dca45201663585d7ea28b4d6021d
rumynin-L <rumynin-L@users.noreply.github.com>
2025-09-25 18:14:47 +0200

    Tirar y recoger la bola fixeado

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+signal ball_thrown
+
 func _physics_process(delta: float) -> void:
 	const SPEED = 400.0
 	
@@ -10,3 +12,10 @@ func _physics_process(delta: float) -> void:
 	if direction != Vector2.ZERO:
 		var target_angle = direction.angle()
 		rotation = lerp_angle(rotation, target_angle, 0.1)
+	
+	if Input.is_action_just_pressed("throw_p1"):
+		throw()
+		
+func throw() -> void:
+	emit_signal("ball_thrown")
+	print("asdkfj")
